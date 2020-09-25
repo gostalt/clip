c2225dc2001d8b05058edd94737eea47352dfaaa
Tom Smith <me@tomm.us>
2020-09-25 21:06:03 +0100

    Add basic generate functionality

--- cmd/generate.go
+++ cmd/generate.go
@@ -7,15 +7,36 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func init() {
+	generateCmd.Flags().IntVarP(&length, "length", "l", 12, "The length of the generated password")
+	generateCmd.Flags().BoolVarP(&upper, "upper", "u", false, "Use uppercase letters in the generated password")
+	generateCmd.Flags().BoolVarP(&digits, "digits", "d", false, "Use digits in the generated password")
+	generateCmd.Flags().BoolVarP(&specials, "specials", "s", false, "Use special characters in the generated password")
+}
+
+var (
+	length   int
+	upper    bool
+	digits   bool
+	specials bool
+)
+
 var generateCmd = &cobra.Command{
-	Use:   "generate",
-	Short: "Generates a password",
-	Args:  cobra.ExactArgs(1),
+	Use:     "generate [website]",
+	Short:   "Generates a password",
+	Example: "pwcli generate github.com --length 18 -dsu",
+	Aliases: []string{"gen"},
+	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		var store password.PasswordStore
 		store = password.NewPasswordFileStore()
 
-		gp := password.Generate(password.GenerateOptions{})
+		gp := password.Generate(password.GenerateOptions{
+			Digits:   digits,
+			Specials: specials,
+			Upper:    upper,
+			Length:   length,
+		})
 
 		store.Set(args[0], gp)
 
--- password/generate.go
+++ password/generate.go
@@ -1,7 +1,56 @@
 package password
 
-type GenerateOptions struct{}
+import (
+	"math/rand"
+	"time"
+)
+
+var (
+	digits   string = "0123456789"
+	specials string = "~=+%^*/()[]{}/!@#$?|"
+	upper    string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lower    string = "abcdefghijklmnopqrstuvwxyz"
+)
+
+type GenerateOptions struct {
+	Digits   bool
+	Specials bool
+	Upper    bool
+
+	Length int
+}
 
 func Generate(opts GenerateOptions) string {
-	return "asdasgfhjyter"
+	rand.Seed(time.Now().UnixNano())
+
+	// By default, add all lower characters
+	enabled := lower
+
+	// Make it impossible to generate an empty password
+	if opts.Length == 0 {
+		opts.Length = 1
+	}
+
+	if opts.Upper {
+		enabled = enabled + upper
+	}
+
+	if opts.Digits {
+		enabled = enabled + digits
+	}
+
+	if opts.Specials {
+		enabled = enabled + specials
+	}
+
+	buf := make([]byte, opts.Length)
+	for i := 0; i < opts.Length; i++ {
+		buf[i] = enabled[rand.Intn(len(enabled))]
+	}
+
+	rand.Shuffle(len(buf), func(i int, j int) {
+		buf[i], buf[j] = buf[j], buf[i]
+	})
+
+	return string(buf)
 }
